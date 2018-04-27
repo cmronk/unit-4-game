93bd2ea0a88d972775fbd9ec55edcfb480a10cdd
cronk <ChristineRonk@Stevens-MacBook-Pro.local>
2018-04-27 00:25:25 -0500

    fixed css and some javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,99 +1,122 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-// Globals
+// variables & counters
 var starting = "";
-var random = "";
+var totalScore = 0;
+$("#total-score").text(totalScore);
+var wins = 0;
+var losses = 0;
 
 // generates starting random number from 19-120
 // (Math.floor(Math.random() * (max-min) + 1)) + min)
-for (var i = 0; i < 1; i++){
-    var starting = Math.floor(Math.random()* (120-19+1)) + 19;
-    console.log(starting);
-    $("#randomNumber").text(starting);
+var starting = Math.floor(Math.random() * 101 + 19);
+// console.log(starting);
+$("#randomNumber").text(starting);
 
+// Generates values for each of the buttons from 1-12 
+var savedNum = (Math.floor(Math.random() * 11 + 1));
+var savedNum1 = (Math.floor(Math.random() * 11 + 1));
+var savedNum2 = (Math.floor(Math.random() * 11 + 1));
+var savedNum3 = (Math.floor(Math.random() * 11 + 1));
+
+// functions
+// scoring
+function win () {
+    alert("You won!");
+    wins++;
+    $("#wins").text(wins);
+    reset ();
 }
-// Generates values for each of the cats from 1-12 
-// (Math.floor(Math.random() * (max-min) + 1)) + min)
+
+function lose () {
+    alert("Sorry, you lost.");
+    losses++;
+    $("#losses").text(losses);
+    reset ();
+}
 
 
-// maybe there is a way to DRY this without the values of the clicks changing in each round?
-var savedNum = (Math.floor(Math.random() * (12-1 + 1)) + 1);
-$("#catBtn").on("click", function(){
-    for (var j = 0; j < 1; j++){
-            // so this just puts the clicked value without adding 
-            // $("#score").text(savedNum);
-
-            // and this adds strings
-            $("#score").append(savedNum);
-    }
-})
 
 
-var savedNum1 = (Math.floor(Math.random() * (12-1 + 1)) + 1);
-$("#catBtn1").on("click", function(){
-    for (var k = 0; k < 1; k++){
-            // so this just puts the clicked value without adding 
-            // $("#score").text(savedNum);
-
-            // and this adds strings
-            $("#score").append(savedNum1);
-    }
-})
-
-var savedNum2 = (Math.floor(Math.random() * (12-1 + 1)) + 1);
-$("#catBtn2").on("click", function(){
-    for (var l = 0; l < 1; l++){
-            // so this just puts the clicked value without adding 
-            // $("#score").text(savedNum);
-
-            // and this adds strings
-            $("#score").append(savedNum2);
-    }
-})
-
-var savedNum3 = (Math.floor(Math.random() * (12-1 + 1)) + 1);
-$("#catBtn3").on("click", function(){
-    for (var m = 0; m < 1; m++){
-            // so this just puts the clicked value without adding 
-            // $("#score").text(savedNum);
-
-            // and this adds strings
-            $("#score").append(savedNum3);
-    }
-})
+// reset
+function reset() {
+    var starting = Math.floor(Math.random() * 101 + 19);
+    $("#randomNumber").text(starting);   
+    var savedNum = (Math.floor(Math.random() * 11 + 1));
+    var savedNum1 = (Math.floor(Math.random() * 11 + 1));
+    var savedNum2 = (Math.floor(Math.random() * 11 + 1));
+    var savedNum3 = (Math.floor(Math.random() * 11 + 1)); 
+    var totalScore = 0;
+    $("#total-score").text(totalScore);
+}
 
 
-})        
+
+    // button on click events
+    $("#lightBtn").on("click", function(){
+        totalScore = savedNum + totalScore;
+        $("#total-score").text(totalScore);
+        if (totalScore === starting)
+        {
+            win ();
+        }
+        else if (totalScore > starting)
+        {
+            lose ();
+        }
+        
+    })
 
 
-// // this section is pseudo coding....
-// // calculates and adds scores from clicking cats and inputs into score container
-// function addClicks() {
+    $("#lightBtn1").on("click", function(){
+        totalScore = savedNum1 + totalScore;
+        $("#total-score").text(totalScore);
+        if (totalScore === starting)
+        {
+            win ();
+        }
+        else if (totalScore > starting)
+        {
+            lose ();
+        }
 
-// }    
+    })
+
+    $("#lightBtn2").on("click", function(){
+        totalScore = savedNum2 + totalScore;
+        $("#total-score").text(totalScore);
+        if (totalScore === starting)
+        {
+            win ();
+        }
+        else if (totalScore > starting)
+        {
+            lose ();
+        }
+
+    })
+
+    $("#lightBtn3").on("click", function(){
+
+        totalScore = savedNum3 + totalScore;
+        $("#total-score").text(totalScore);
+        if (totalScore === starting)
+        {
+            win ();
+        }
+        else if (totalScore > starting)
+        {
+            lose ();
+
+        }
+
+    })  
+
+    })
 
 
-// function scoring() {
-// // if score === starting random number, user wins
-//     if (parseInt(score) > starting) {
-//         alert("Winner!");
-//         wins++;
 
 
-//     }
-// // if score > starting random number, user loses
-// // if score < starting random number, user must continue play
 
-// }
-     
-// // reset when user wins/loses or gamestart
-// function reset() {
-
-// }
-
-// // start game
-// function startGame() {
-
-// }
 
 
